626551d96cf0596cfc6db8c05e7fe015b2656a40
Alvardud <alvaro_dudutex@outlook.es>
2019-11-26 23:02:00 -0400

    add details and change concept

--- lib/ui/pages/product_details/widgets/aditional_information.dart
+++ lib/ui/pages/product_details/widgets/aditional_information.dart
@@ -2,6 +2,7 @@ import 'package:aurora_fruts/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:aurora_fruts/utils/config.dart' as config;
 import 'package:aurora_fruts/data/constants.dart' as constant;
+import 'package:aurora_fruts/data/example/details_description.dart' as details;
 
 class AditionalInformation extends StatefulWidget {
   final Product product;
@@ -20,13 +21,11 @@ class _AditionalInformationState extends State<AditionalInformation>
 
   Widget _containerAditional(int picker) {
     if (picker == 2) {
-      return Container();
+      return Ingredients(ingredients: widget.product.ingredients);
     } else if (picker == 1) {
-      return Container();
+      return Notes(notes: widget.product.observer);
     } else {
-      return Description(
-        description: widget.product.description,
-      );
+      return Description(description: widget.product.description);
     }
   }
 
@@ -78,7 +77,7 @@ class _AditionalInformationState extends State<AditionalInformation>
               tabs: <Widget>[
                 Tab(child: Text('Descripción')),
                 Tab(child: Text('Notas')),
-                Tab(child: Text('Comentarios')),
+                Tab(child: Text('Ingredientes')),
               ],
             ),
           ),
@@ -120,11 +119,114 @@ class Description extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical:8.0,horizontal: 16.0),
-        child:
-            Text('"${description[0].toUpperCase()}${description.substring(1)}"',style: TextStyle(color: Colors.grey[600]),),
-      ),
+      child: description != null
+          ? Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: Center(
+                      child: Icon(details.iconCardSection[0], size: 64.0)),
+                ),
+                Flexible(
+                  flex: 4,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    child: Text(
+                      '"${description[0].toUpperCase()}${description.substring(1)}"',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Center(
+              child: Text(
+                'No hay descripción\ndisponible',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey[500], fontSize: 24.0),
+              ),
+            ),
+    );
+  }
+}
+
+class Notes extends StatelessWidget {
+  final String notes;
+  Notes({this.notes});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: notes != null
+          ? Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 4,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    child: Text(
+                      '"${notes[0].toUpperCase()}${notes.substring(1)}"',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Center(
+                      child: Icon(details.iconCardSection[1], size: 64.0)),
+                ),
+              ],
+            )
+          : Center(
+              child: Text(
+                'No hay notas\ndisponibles',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey[500], fontSize: 24.0),
+              ),
+            ),
+    );
+  }
+}
+
+class Ingredients extends StatelessWidget {
+  final List<String> ingredients;
+  Ingredients({this.ingredients});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ingredients != null
+          ? Row(
+              children: <Widget>[
+                Flexible(
+                    flex: 2,
+                    child: Center(
+                        child: Icon(details.iconCardSection[2], size: 64.0))),
+                Flexible(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        for (int i = 0; i < ingredients.length; i++)
+                          Text(
+                            "*\t\t${ingredients[i]}",
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 18.0,
+                                fontStyle: FontStyle.italic),
+                          )
+                      ],
+                    )),
+              ],
+            )
+          : Center(
+              child: Text(
+                'Información no\ndisponible',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey[500], fontSize: 24.0),
+              ),
+            ),
     );
   }
 }
