38ebc982ab9b2f25f460f69f6d67830438de7eb4
Marco Leonardini <marco_leonardini@hotmail.com>
2019-11-30 12:12:33 -0400

    merged

--- lib/data/example/products.dart
+++ lib/data/example/products.dart
@@ -9,11 +9,11 @@ List<Product> products = [
 
 Product productExample1 = Product(
   name: 'té de manzana',
-  discount: 8.42,
+  discount: 1.42,
   weight: '500 ml',
   description:
       'Un tipo de jugo de fruta que se hace de la pulpa de manzanas procesadas para su jugo. Es un líquido muy claro del que se ha eliminado la pulpa. Este jugo a menudo se usa para dar sabor a las carnes, o como ingrediente en aderezos y salsas, agregando un sabor dulce a manzana.',
-  price: 10.62,
+  price: 1.62,
   provider: 'fresh fruits inc.',
   labels: ['jugo', 'destilado', 'bebida caliente'],
   ingredients: ['pulpa de manzana', 'azucar', 'canela', 'agua'],
@@ -28,7 +28,7 @@ Product productExample2 = Product(
   weight: '800 gr',
   description:
       'Más que un postre rico y crujiente, estas galletas de avena son beneficiosas para llevar a cabo una buena digestión. Además, estas galletas nos aportan mucha energía sin abusar de los carbohidratos.',
-  price: 8.40,
+  price: 3.40,
   provider: 'la estancia',
   labels: ['producto integral', 'galletas'],
   observer: 'mantener en un envase biodegradable, como bolsas de papel',
@@ -48,13 +48,14 @@ Product productExample2 = Product(
 
 Product productExample3 = Product(
   name: 'almendras',
-  discount: 10.50,
+  discount: 4.50,
   weight: '500 gr',
   description:
       'Las almendras proporcionan mucha energía, proteínas, minerales, vitaminas y fibra. Además, la mayoría de las grasas que contienen son insaturadas y reducen el colesterol en la sangre.',
-  price: 11.00,
-  provider: 'la colmena',
-  labels: ['frutos secos', 'producto natural', 'semillas'],
+  price: 8.20,
+  provider: 'fresh fruits inc.',
+  labels: ['jugo', 'destilado', 'bebida caliente'],
+  ingredients: ['pulpa de manzana', 'azucar', 'canela', 'agua'],
   images: [
     'https://mlstaticquic-a.akamaihd.net/almendras-x-12-kiloventa-min1-kg-leer-descripcion-D_NQ_NP_952483-MLU27371602157_052018-F.jpg',
   ],
@@ -62,11 +63,11 @@ Product productExample3 = Product(
 
 Product productExample4 = Product(
   name: 'zumo de tomate',
-  discount: 12.0,
+  discount: 2.3,
   weight: '600 ml',
   description:
       'El jugo de tomate o zumo de tomate es un preparado procedente del jugo de los tomates que se suele emplear como bebida. ',
-  price: 12.5,
+  price: 2.5,
   provider: 'fresh fruits inc.',
   labels: ['zumo', 'vegetales', 'bebida conservada'],
   ingredients: [
@@ -79,7 +80,8 @@ Product productExample4 = Product(
     'azucar',
     'hielo'
   ],
-  observer: 'mantener el producto a una temperatura al rededor de los 10°C a 15°C',
+  observer:
+      'mantener el producto a una temperatura al rededor de los 10°C a 15°C',
   images: [
     'https://cdn.cookmonkeys.es/recetas/medium/zumo-de-tomate-preparado-en-thermomix-13665.jpg',
   ],
